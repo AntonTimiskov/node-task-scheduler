5c25a2813d89b7696a7824ced61c446a7d921c54
OlegAlexandrov <Oleg.Alexandrov@quest.com>
2018-04-20 12:14:40 +0000

    use new version

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t antontimiskov/node-task-scheduler:1.0.1 -f ./Dockerfile .
-docker build -t antontimiskov/node-task-scheduler:1.0.1-tests -f ./Dockerfile-tests  .
+docker build -t antontimiskov/node-task-scheduler:1.0.2-k8s -f ./Dockerfile .
+# docker build -t antontimiskov/node-task-scheduler:1.0.2-k8s-tests -f ./Dockerfile-tests  .
