42d1c31e7248a9447ec9c276411b217933c35a9a
Pavel Timofeev <pavel.timofeev@quest.com>
2018-04-24 13:11:51 +0000

    setup tests running

--- tests.at.sh
+++ tests.at.sh
@@ -5,8 +5,9 @@ NAME=fitnesse_node_task_scheduler
 
 docker rm -f $NAME 2> /dev/null
 
-docker run -it \
+docker run -d \
     -v $(pwd)/tests/AT/FitNesseRoot:/FitNesseRoot \
+    -v $(pwd)/tests/AT/fixtures:/FitNesseRoot/Scripts \
     --name=$NAME \
     -p $PORT:8081 \
     mikeplavsky/docker-waferslim
